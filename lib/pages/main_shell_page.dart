7a601a0d37ed61987bd86189e992ed4ba4dfa3ac
Manu.A <manuel.arra@outlook.com>
2026-02-16 19:58:39 +0100

    Add main shell with home and statistics bottom navigation

---
+++ lib/pages/main_shell_page.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+import 'package:monthly_budget/pages/home_page.dart';
+import 'package:monthly_budget/pages/stats_page.dart';
+
+class MainShellPage extends StatefulWidget {
+  const MainShellPage({super.key});
+
+  @override
+  State<MainShellPage> createState() => _MainShellPageState();
+}
+
+class _MainShellPageState extends State<MainShellPage> {
+  int _selectedIndex = 0;
+  late final List<Widget> _pages;
+
+  @override
+  void initState() {
+    super.initState();
+    _pages = const [HomePage(), StatsPage()];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(index: _selectedIndex, children: _pages),
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: _selectedIndex,
+        onDestinationSelected: (index) {
+          if (index == _selectedIndex) return;
+          setState(() => _selectedIndex = index);
+        },
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(Icons.home_outlined),
+            selectedIcon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.bar_chart_outlined),
+            selectedIcon: Icon(Icons.bar_chart),
+            label: 'Statistics',
+          ),
+        ],
+      ),
+    );
+  }
+}
